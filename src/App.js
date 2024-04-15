57821c382bdbd260850d0f000fbf5a11f0c9f05e
Sumit Awasthi <sumawajavaa@gmail.com>
2024-04-15 23:57:18 +0530

    my first commits

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Hi Sumit!</p>
+      <p>Hi Sumit! Awasthi</p>
       </header>
     </div>
   );
